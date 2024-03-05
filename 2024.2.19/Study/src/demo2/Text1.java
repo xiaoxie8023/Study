6734fed845a3fffa41884e9098d02b406f5240bb
xie-zhonghan <2720583780@qq.com>
2024-03-05 20:48:14 +0800

    刷题

--- 2024.2.19/Study/src/demo2/Text1.java
+++ 2024.2.19/Study/src/demo2/Text1.java
@@ -6,6 +6,11 @@ package demo2;/**
  * Time: 20:31
  */
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
 /** 刷题
  * * @author xiaoxie
  * @date 2024年02月27日 20:31
@@ -187,5 +192,103 @@ public class Text1 {
         if(l2 == null) curr.next = l1;
         return dummy.next;
     }
-
+    /** 力扣 860 柠檬水找零
+     * https://leetcode.cn/problems/lemonade-change/
+     * 使用贪心算法
+     * 主要的思想就是
+     * 1.如果顾客支付的是5元,那么就收下
+     * 2.如果是10元的话,就需要找零5元,
+     * 3.如果为20的话,找零有两种方法
+     * 3.1 5+10
+     * 3.2 5+5+5
+     * 所以解决这道题的关键就是使用贪心算法优先使用10元来找零
+     * 贪心思想在这道题中的体现也许就是，“把最不常用的找出去（面额大的）“
+     * 因为两个5元比一个10元更加灵活，适用的场景更多
+     * @author xiaoxie
+     * @date 2024/3/5 16:19
+     * @param bills
+     * @return boolean
+     */
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0,ten = 0;
+        for(int money : bills) {
+            if(money == 5) {
+                five++;
+            }else if(money == 10) {
+                if(five == 0) {
+                    return false;
+                }else {
+                    five--;
+                    ten++;
+                }
+            }else {
+                //贪心的过程
+                if(five != 0 && ten != 0) {
+                    five--;
+                    ten--;
+                }else if(five >= 3) {
+                    five -= 3;
+                }else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    /** 力扣 328 奇偶链表
+     * https://leetcode.cn/problems/odd-even-linked-list/description/
+     * 大体思路就是 先把奇数节点串在一起,然后把偶数节点串在一起
+     * 之后在把两个链表串在一起就可以了
+     *  时间复杂度:O(n)
+     *  空间复杂度:O(1) 就在原链表上操作
+     * @author xiaoxie
+     * @date 2024/3/5 16:48
+     * @param head
+     * @return demo2.Text1.ListNode
+     */
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null) {
+            return null;
+        }
+        ListNode cur = head.next;//偶数节点的头结点
+        ListNode l1 = head;//奇数节点的头结点
+        ListNode l2 = cur;//偶数节点
+        while(l2 != null && l2.next != null) {
+            l1.next = l2.next;//奇数节点的下一个节点为原先偶数节点的下一个节点
+            l2.next = l1.next.next;//同理
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        l1.next = cur;
+        return head;
+    }
+    /** 力扣 496. 下一个更大元素 I
+     *  https://leetcode.cn/problems/next-greater-element-i/description/
+     *  主要原理
+     *  单调栈+哈希表 (Deque + Map )
+     *  这里要记住:
+     *  一但要求下一个更大的元素，就是用单调栈解
+     * @author xiaoxie
+     * @date 2024/3/5 17:11
+     * @param nums1
+     * @param nums2
+     * @return int[]
+     */
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer,Integer> ans = new HashMap<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+        for(int i = nums2.length-1; i>= 0;--i) {
+            int num = nums2[i];
+            while(!dq.isEmpty() && num >= dq.peek()) {
+                dq.pop();
+            }
+            ans.put(num,dq.isEmpty() ? -1 : dq.peek());
+            dq.push(num);
+        }
+        int[] res = new int[nums1.length];
+        for(int i = 0; i < nums1.length;++i) {
+            res[i] = ans.get(nums1[i]);
+        }
+        return res;
+    }
 }
